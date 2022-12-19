90fd1dfad780a42c4a1d6bfa65bdd2709900b2af
Dylan Francis <dylfrancis505@gmail.com>
2022-12-19 13:40:05 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, remove1, remove2 = null) {
+  const index1 = array.indexOf(remove1);
+  const index2 = array.indexOf(remove2);
 
+  array.splice(index1, 1);
+  if (remove2 !== null) {
+    array.splice(index2, 1);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
